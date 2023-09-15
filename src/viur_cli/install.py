07e82cbae72036d2de768545ba316f990c81f2cf
Sven Eberth <se@mausbrand.de>
2023-09-15 20:02:04 +0200

    feat: Add targets to all install softwares and ensure a specific version is correctly working
    Furthermore refacoring the entire code, remove duplicates and use modern
    `Pathlib`.

--- src/viur_cli/install.py
+++ src/viur_cli/install.py
@@ -1,52 +1,80 @@
-import click, os, zipfile, shutil, urllib, json
-from . import cli, get_config, echo_error, write_config
+import os
+import shutil
+import zipfile
+from pathlib import Path
 from urllib.request import urlretrieve
+
+import click
+import requests
+
+from . import cli, echo_error, echo_info, get_config, write_config
+
+REPOS = {
+    "vi": ("viur-framework/viur-vi", "viur-vi.zip"),
+    "admin": ("viur-framework/vi-admin", "viur-vi.zip"),
+    "scriptor": ("viur-framework/viur-scriptor", "source.zip"),
+}
+
+
+def get_version_info(software: str, version: str) -> tuple[str, str]:
+    """Validate the version name and return the download url.
+
+    :param software: The key of a software in REPOS
+    :param version: The version number entered by the user
+    :return: A pair of the real version number and the download url
+    """
+    repo, download_name = REPOS[software]
+
+    if version[0] == "v":
+        echo_info("Version should be provided without a leading \"v\"!")
+        version = version[1:]  # normalize it!
+
+    if version == "latest":
+        url = f"https://api.github.com/repos/{repo}/releases/latest"
+    else:
+        url = f"https://api.github.com/repos/{repo}/releases/tags/v{version}"
+    if not (req := requests.get(url)).ok:
+        echo_error(f"Error while fetching version info (request failed): {req.status_code} {req.reason}")
+        real_version = None  # Unknown
+    else:
+        # It's a validated and real existing version, save it to our config
+        real_version: str = req.json()["name"]
+        project_config = get_config()
+        project_config["default"][software] = real_version.lstrip("v")
+        write_config(project_config)
+
+    if not real_version and version == "latest":
+        download_url = f"https://github.com/{repo}/releases/latest/download/{download_name}"
+    else:
+        # if the request before didn't failed, we use always the version
+        tag = real_version if real_version else f"v{version}"
+        download_url = f"https://github.com/{repo}/releases/download/{tag}/{download_name}"
+
+    return real_version, download_url
 
 
 @cli.group()
 def install():
     """Install VIUR features"""
 
+
 @install.command()
 @click.argument("version", default="latest")
-@click.option('--next', '-n', 'next_',  is_flag=True, default=False)
-@click.option('--target', '-t',  default="vi")
+@click.option("--next", "-n", "next_", is_flag=True, default=False)
+@click.option("--target", "-t", default="vi")
 def vi(version, target, next_):
-    """Install VI administration interface."""
-
+    """Install vi — the legacy VIUR administration interface."""
     if next_:
-        utils.echo_info("DEPRECATED please use: viur install admin")
-        downloadadmin()
-        return 0
+        echo_info("DEPRECATED please use: viur install admin")
+        return downloadadmin(version, target)
 
-    """download latest vi or a specific version"""
     projectConfig = get_config()
-    distFolder = projectConfig["default"]["distribution_folder"]
+    dist_folder = projectConfig["default"]["distribution_folder"]
 
-    viRepo = "https://github.com/viur-framework/viur-vi"
-    viPath = os.path.join(distFolder, target)
+    real_version, download_url = get_version_info("vi", version)
 
-    tempZipFile = "./vi.zip"
-
-    if version == "latest":
-        vibaseUrl = f"{viRepo}/releases/latest/download/viur-vi.zip"
-    else:
-        vibaseUrl = f"{viRepo}/releases/download/v{version}/viur-vi.zip"
-
-    def get_version_info(version):
-        url = "https://api.github.com/repos/viur-framework/viur-vi/releases"
-        if version == "latest":
-            try:
-                resp = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
-                projectConfig["default"]["vi"] = resp[0]["name"][1:]
-                write_config(projectConfig)
-            except:
-                echo_error("Error while fetching version info")
-        else:
-            projectConfig["default"]["vi"] = f'{version}'
-            write_config(projectConfig)
-
-    get_version_info(version)
+    vi_path = Path(dist_folder, target)
+    tmp_zip_file = Path("vi.zip")
 
     def step_label(step):
         if step == 1:
@@ -61,105 +89,75 @@ def vi(version, target, next_):
     with click.progressbar([1, 2, 3, 4], label="updating vi...", item_show_func=step_label) as bar:
         for element in bar:
             if element == 1:
-                urlretrieve(vibaseUrl, tempZipFile)
+                urlretrieve(download_url, tmp_zip_file)
             elif element == 2:
-                if os.path.exists(viPath):
-                    shutil.rmtree(viPath)
+                if os.path.exists(vi_path):
+                    shutil.rmtree(vi_path)
             elif element == 3:
-                with zipfile.ZipFile(tempZipFile) as zip_f:
-                    zip_f.extractall(viPath)
+                with zipfile.ZipFile(tmp_zip_file) as zip_f:
+                    zip_f.extractall(vi_path)
             elif element == 4:
-                os.remove(tempZipFile)
+                tmp_zip_file.unlink()
                 bar.label = "updated successful"
+
 
 @install.command()
 @click.argument("version", default="latest")
-def admin(version):
-    downloadadmin(version)
+@click.option("--target", "-t", default="vi")
+def admin(version, target):
+    """Install admin — the new VIUR administration interface."""
+    return downloadadmin(version, target)
 
 
-def downloadadmin(version="latest"):
-    """download latest vi or a specific version"""
+def downloadadmin(version: str, target: str):
+    """download latest vi-admin or a specific version"""
     projectConfig = get_config()
-    distFolder = projectConfig["default"]["distribution_folder"]
+    dist_folder = projectConfig["default"]["distribution_folder"]
 
-    viRepo = "https://github.com/viur-framework/vi-admin"
-    viPath = os.path.join(distFolder, "vi")
-    tempZipFile = "./vi.zip"
+    real_version, download_url = get_version_info("admin", version)
 
-    if version == "latest":
-        vibaseUrl = f"{viRepo}/releases/latest/download/viur-vi.zip"
-    else:
-        vibaseUrl = f"{viRepo}/releases/download/v{version}/viur-vi.zip"
-
-    def get_version_info(version):
-        url = "https://api.github.com/repos/viur-framework/viur-vuevi/releases"
-        if version == "latest":
-            try:
-                resp = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
-                projectConfig["default"]["vi"] = resp[0]["name"]
-                write_config(projectConfig)
-            except:
-                echo_error("Error while fetching version info")
-        else:
-            projectConfig["default"]["vi"] = f'{version}'
-            write_config(projectConfig)
-
-    get_version_info(version)
+    admin_path = Path(dist_folder, target)
+    tmp_zip_file = Path("vi-admin.zip")
 
     def step_label(step):
         if step == 1:
-            return f"downloading next vi..."
+            return f"downloading admin..."
         elif step == 2:
-            return f"remove old vi..."
+            return f"remove old admin..."
         elif step == 3:
-            return f"extracting new vi..."
+            return f"extracting new admin..."
         elif step == 4:
             return f"success!"
 
     with click.progressbar([1, 2, 3, 4], label="updating admin...", item_show_func=step_label) as bar:
         for element in bar:
             if element == 1:
-                urlretrieve(vibaseUrl, tempZipFile)
+                urlretrieve(download_url, tmp_zip_file)
             elif element == 2:
-                if os.path.exists(viPath):
-                    shutil.rmtree(viPath)
+                if os.path.exists(admin_path):
+                    shutil.rmtree(admin_path)
             elif element == 3:
-                with zipfile.ZipFile(tempZipFile) as zip_f:
-                    zip_f.extractall(viPath)
+                with zipfile.ZipFile(tmp_zip_file) as zip_f:
+                    zip_f.extractall(admin_path)
             elif element == 4:
-                os.remove(tempZipFile)
+                tmp_zip_file.unlink()
                 bar.label = "updated successful"
 
 
 @install.command()
 @click.argument("version", default="latest")
-def scriptor(version):
+@click.option("--target", "-t", default="scriptor")
+def scriptor(version, target):
     """Install scriptor IDE."""
     projectConfig = get_config()
     dist_folder = projectConfig["default"]["distribution_folder"]
 
-    scriptor_repo = "https://github.com/viur-framework/viur-scriptor"
-    scriptor_path = os.path.join(dist_folder, "scriptor")
-    tmp_zip_file = "./scriptor.zip"
-    scriptor_base_url = f"{scriptor_repo}/releases/latest/download/source.zip"
+    real_version, download_url = get_version_info("scriptor", version)
 
-    def get_version_info(version):
-        url = "https://api.github.com/repos/viur-framework/viur-scriptor/releases"
-        if version == "latest":
-            try:
-                resp = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
-                projectConfig["default"]["scriptor"] = resp[0]["name"]
-                write_config(projectConfig)
-            except:
-                echo_error("Error while fetching version info")
-        else:
-            projectConfig["default"]["scriptor"] = f'{version}'
-            write_config(projectConfig)
+    scriptor_path = Path(dist_folder, target)
+    tmp_zip_file = Path("scriptor.zip")
 
-    get_version_info(version)
-
-    def step_label(step):
+    def step_label(step: int) -> str:
         if step == 1:
             return f"downloading scriptor..."
         elif step == 2:
@@ -171,9 +169,10 @@ def scriptor(version):
         for element in bar:
             match element:
                 case 1:
-                    urlretrieve(scriptor_base_url, tmp_zip_file)
+                    urlretrieve(download_url, tmp_zip_file)
                 case 2:
-                    zipfile.ZipFile(tmp_zip_file).extractall()
+                    with zipfile.ZipFile(tmp_zip_file) as zip_f:
+                        zip_f.extractall(scriptor_path)
                 case 3:
-                    os.remove(tmp_zip_file)
+                    tmp_zip_file.unlink()
                     bar.label = "updated successful"
