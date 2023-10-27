df78245621ca8a5495230d974212212feeb1cb8a
Christos <ca@mausbrand.com>
2023-10-27 17:09:52 +0200

    started documentation for build.py

--- src/viur_cli/build.py
+++ src/viur_cli/build.py
@@ -10,11 +10,35 @@ from . import cli, conf, utils
 def _build(cfg, name, build_cfg, additional_args):
     """Internal function to perform steps required for a given build configuration.
 
-    :param cfg: The project configuration (default, or project-specific)
-    :param name: Name of the app to build.
-    :param build_cfg: The build-cfg for the specified app.
-    :param additional_args: List of additional arguments.
+    This internal function is responsible for building an application according to the specified build configuration.
+    It can handle two types of builds:
+        - npm: For building Node.js applications using npm package manager.
+        - exec: For executing custom commands specified in the build configuration.
+
+    :param cfg: dict
+        The project configuration, which can be either the default configuration or project-specific configuration.
+    :param name: str
+        The name of the application to build.
+    :param build_cfg: dict
+        The build configuration for the specified application. This configuration includes the "kind" (build type),
+        "source" (source folder or path), and "command" (the build command to execute).
+    :param additional_args: list
+        A list of additional arguments that can be passed to the build process.
+    :raises Exception:
+        Raises an exception if an unknown build "kind" is encountered in the build configuration.
+
+    The `_build` function performs the following steps based on the build "kind":
+    - For "npm" builds, it changes the current directory to the specified source folder, installs npm dependencies, and
+    runs the specified npm command.
+    - For "exec" builds, it directly executes the specified custom command.
+
+    Note:
+    - Ensure that the required dependencies for the specified build type are installed before calling this function.
+    - The function is responsible for echoing informative messages during the build process.
+
+    :return: None
     """
+
     utils.echo_info(f"""- {build_cfg["kind"]} {name}""")
 
     match build_cfg["kind"]:
@@ -37,12 +61,40 @@ def _build(cfg, name, build_cfg, additional_args):
 
 
 def _clean(cfg, name, build_cfg):
-    """Internal function to perform steps required to clean a given configuration.
-
-    :param cfg: The project configuration (default, or project-specific)
-    :param name: Name of the app to clean.
-    :param build_cfg: The build-cfg for the specified app.
     """
+        Perform steps required to clean a given build configuration.
+
+        This internal function is responsible for cleaning the artifacts and files generated during the build process
+        for a specified application. It supports two types of cleaning methods:
+            - npm: For cleaning Node.js applications built with npm package manager.
+            - exec: For executing custom clean commands specified in the build configuration.
+
+        :param cfg: dict
+            The project configuration, which can be either the default configuration or a
+            project-specific configuration.
+
+        :param name: str
+            The name of the application to clean.
+
+        :param build_cfg: dict
+            The build configuration for the specified application. This configuration includes the "kind" (build type),
+            "target" (target folder to clean), "source" (source folder or path), "clean" (custom clean command).
+
+        :raises Exception:
+            Raises an exception if an unknown build "kind" is encountered in the build configuration.
+
+        The `_clean` function performs the following steps based on the build "kind":
+        - For "npm" builds, it deletes the specified target folder, and if available, it executes a custom "clean"
+        command.
+        - For "exec" builds, it executes the custom clean command provided in the build configuration.
+
+        Note:
+        - Ensure that the necessary clean configurations are defined in the build configuration.
+        - The function provides informative messages during the cleaning process.
+
+        :return: None
+        """
+
     utils.echo_info(f"""- cleaning {build_cfg["kind"]} {name}""")
 
     match build_cfg["kind"]:
@@ -81,7 +133,30 @@ def build():
 @click.argument("name", default='develop')
 @click.argument("additional_args", nargs=-1)
 def release(name, additional_args):
-    """Build of all relevant apps to deploy this project."""
+    """
+    Build all relevant applications to deploy this project.
+
+    This command is used to build all relevant applications necessary for deploying the project. It allows you to
+    specify the project configuration to use and any additional arguments to pass to the build process.
+
+    :param name: str, default: 'develop'
+        The name of the project configuration to use for building. This configuration can be either 'default' or a
+        project-specific configuration.
+
+    :param additional_args: tuple
+        Additional arguments that can be passed to the build process. These arguments are passed to the underlying
+        build functions.
+
+    The `release` command loads the specified project configuration, which includes build configurations for
+    individual applications. It then iterates through the applications and executes the build process for each one
+    using the `_build` function.
+
+    Note:
+    - Ensure that the specified project configuration exists.
+    - Additional arguments can be used to customize the build process.
+
+    :return: None
+    """
 
     projectConfig = conf.get_config()
 
@@ -103,7 +178,28 @@ def release(name, additional_args):
 @click.argument("appname")
 @click.argument("additional_args", nargs=-1)
 def app(appname, additional_args):
-    """Build specific app."""
+    """
+    Build a specific application.
+
+    This function is used to build a specific application as defined in your project's configuration. It allows you to
+    specify the name of the application to build and any additional arguments to pass to the build process.
+
+    :param appname: str
+        The name of the application to build. It should correspond to an application defined in your project's
+        configuration.
+
+    :param additional_args: tuple
+        Additional arguments that can be passed to the build process for the specified application.
+
+    The `app` function loads the default project configuration, selects the specified application's build configuration,
+    and then executes the build process using the `_build` function.
+
+    Note:
+    - Ensure that the specified application name is valid and defined in your project's configuration.
+    - Additional arguments can be used to customize the build process for this specific application.
+
+    :return: None
+    """
 
     projectConfig = conf.get_config()
 
