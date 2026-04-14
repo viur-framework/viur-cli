viur-framework/viur-cli

commit 5737d4153f15967ec855a76b3b3e92322ca1ca5d

fix: Script add (#200)
Change order of parameter for script add call

Co-authored-by: Kadir Balku <111311265+KadirBalku@users.noreply.github.com>

src/viur_cli/scriptor/cli.py
@@ -107,10 +107,8 @@ def check_session(ctx: click.Context):
         click.echo("Invalid session, please run `viur script setup` again. okay ?")
         ctx.invoke(setup)
         ctx.close()
-    # init modules
-    get_modules()
 
-
+        
 @script.command()
 @click.option('--force', default=False, help='Force replace files from server in local working directory')
 @click.pass_context
@@ -293,7 +291,7 @@ def push(ctx: click.Context, force: bool, watch: bool):
                                 f.write(args["script"])
 
                     click.echo(f"Push {_real_file}")
-                    await tree.add(_type, args)
+                    await tree.add(args, skel_type=_type)
 
     if watch:
         print("Watching...")

commit 5f3694f4fbf9fedb8957e26644409bd535b7170a

fix: Add get_modules in check_session (#208)
Fixes
```
Traceback (most recent call last):
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/bin/viur", line 7, in <module>
    sys.exit(cli())
             ~~~^^
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 1161, in __call__
    return self.main(*args, **kwargs)
           ~~~~~~~~~^^^^^^^^^^^^^^^^^
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 1082, in main
    rv = self.invoke(ctx)
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 1697, in invoke
    return _process_result(sub_ctx.command.invoke(sub_ctx))
                           ~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 1697, in invoke
    return _process_result(sub_ctx.command.invoke(sub_ctx))
                           ~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 1443, in invoke
    return ctx.invoke(self.callback, **ctx.params)
           ~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/core.py", line 788, in invoke
    return __callback(*args, **kwargs)
  File "/.local/share/virtualenvs/ag-dev-lcuCThDg/lib/python3.13/site-packages/click/decorators.py", line 33, in new_func
    return f(get_current_context(), *args, **kwargs)
  File "/Schreibtisch/work/ag-dev/viur-cli/src/viur_cli/scriptor/cli.py", line 163, in pull
    asyncio.new_event_loop().run_until_complete(main())
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^
  File "/.pyenv/versions/3.13.11/lib/python3.13/asyncio/base_events.py", line 725, in run_until_complete
    return future.result()
           ~~~~~~~~~~~~~^^
  File "/Schreibtisch/work/ag-dev/viur-cli/src/viur_cli/scriptor/cli.py", line 123, in main
    modules = get_modules()
  File "/Schreibtisch/work/ag-dev/viur-cli/src/viur_cli/scriptor/cli.py", line 26, in get_modules
    asyncio.new_event_loop().run_until_complete(_modules.init())
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^
  File "/.pyenv/versions/3.13.11/lib/python3.13/asyncio/base_events.py", line 701, in run_until_complete
    self._check_running()
    ~~~~~~~~~~~~~~~~~~~^^
  File "/.pyenv/versions/3.13.11/lib/python3.13/asyncio/base_events.py", line 639, in _check_running
    raise RuntimeError(
        'Cannot run the event loop while another loop is running')
RuntimeError: Cannot run the event loop while another loop is running
```
Caused by
https://github.com/viur-framework/viur-cli/pull/200

src/viur_cli/scriptor/cli.py
@@ -107,8 +107,9 @@ def check_session(ctx: click.Context):
         click.echo("Invalid session, please run `viur script setup` again. okay ?")
         ctx.invoke(setup)
         ctx.close()
+    # init modules
+    get_modules()
 
-        
 @script.command()
 @click.option('--force', default=False, help='Force replace files from server in local working directory')
 @click.pass_context